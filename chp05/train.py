f0aa6fd17a364075688f8b92eb7fe01cf98e571f
makotos-nlproc <70269105+makotos-nlproc@users.noreply.github.com>
2020-10-09 22:54:26 +0900

    p101 train.py途中まで

--- chp05/train.py
+++ chp05/train.py
@@ -8,3 +8,13 @@ from utils import load_dataset, train_and_eval
 def main():
     x, y = load_dataset('data/amazon_reviews_multilingual_JP_v1_00.tsv', n=5000)
     
+    print('Tokenization')
+    x = [clean_html(text, strip=True) for text in x]
+    x = [' '.join(tokenize(text)) for text in x]
+    x_train, x_test, y_train. y_test = train_test_split(x, y,
+                                                        test_size=0.2,
+                                                        random_state=42)
+    
+
+    print('Binary')
+    
