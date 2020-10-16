1ed608c4dcb61de77096c47570bd4f60dafe072f
makotos-nlproc <70269105+makotos-nlproc@users.noreply.github.com>
2020-10-16 23:44:04 +0900

    p128, import途中

---
+++ chp06/cross_validation.py
@@ -0,0 +1,4 @@
+from sklearn.feature_extraction.text import TfidfVectorizer
+from sklearn.linear_model import LogisticRegression
+from sklearn.model_selection import train_test_split, cross_val_score
+from sklearn.metrics import accuracy_score
