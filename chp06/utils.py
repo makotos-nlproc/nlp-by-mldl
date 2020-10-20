8fc4a05c0b1aee8379a49feac9f9e252a9214534
makotos-nlproc <70269105+makotos-nlproc@users.noreply.github.com>
2020-10-20 23:47:38 +0900

    p133 続き

--- chp06/utils.py
+++ chp06/utils.py
@@ -2,5 +2,6 @@ import numpy as np
 import matplotlib.pyplot as plt
 from sklearn.model_selection import learning_curve
 
-def plot_learning_curve():
+def plot_learning_curve(title):
     plt.figure()
+    plt.title(title)
