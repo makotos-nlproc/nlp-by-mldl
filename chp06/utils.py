2de094488a4a785ea293d8b1d89cbbf2bac8a476
makotos-nlproc <70269105+makotos-nlproc@users.noreply.github.com>
2020-10-21 23:36:16 +0900

    p133引数を追加

--- chp06/utils.py
+++ chp06/utils.py
@@ -2,6 +2,7 @@ import numpy as np
 import matplotlib.pyplot as plt
 from sklearn.model_selection import learning_curve
 
-def plot_learning_curve(title):
+def plot_learning_curve(estimater, title, X, y, ylim=None, cv=None,
+                        n_jobs=-1, train_size=np.linspace(.1, 1.0, 5)):
     plt.figure()
     plt.title(title)
