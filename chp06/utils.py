1d9d9d5667dab18f83871a7fc157572f34aea520
makotos-nlproc <70269105+makotos-nlproc@users.noreply.github.com>
2020-10-22 23:47:38 +0900

    p133

--- chp06/utils.py
+++ chp06/utils.py
@@ -3,6 +3,14 @@ import matplotlib.pyplot as plt
 from sklearn.model_selection import learning_curve
 
 def plot_learning_curve(estimater, title, X, y, ylim=None, cv=None,
-                        n_jobs=-1, train_size=np.linspace(.1, 1.0, 5)):
+                        n_jobs=-1, train_sizes=np.linspace(.1, 1.0, 5)):
     plt.figure()
     plt.title(title)
+
+    if ylim is not None:
+        plt.ylim(*ylim)
+    
+    plt.xlabel('Training examples')
+    plt.ylabel('Score')
+
+    
